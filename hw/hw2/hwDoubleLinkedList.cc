a91eb59f1b919aa7dce62e3ec664f969404e1930
Danny Desktop <dannyzatko@gmail.com>
2022-10-02 23:09:42 -0400

    finished hw2

--- hw/hw2/hwDoubleLinkedList.cc
+++ hw/hw2/hwDoubleLinkedList.cc
@@ -32,68 +32,116 @@ public:
 	DoubleLinkedList& operator =(const DoubleLinkedList& orig) = delete;
 
 	// implement to print out the list
-	friend ostream& operator <<(ostream& s, const DoubleLinkedList& list) {}
+	friend ostream& operator <<(ostream& s, const DoubleLinkedList& list) {
+		for (Node* temp = list.head; temp != nullptr; temp = temp->next)
+      s << temp->val << ' ';
+    return s;
+	}
 
 	void addStart(int v) {
+		Node* temp;
 		//Case if list is empty
-			if(head == nullptr){
-			temp = new Node(nullptr, nullptr, v)
+		if(head == nullptr){ // O(1)
+			temp = new Node(nullptr, nullptr, v);
 			head = temp;
 			tail = temp;
-			delete temp;
 		}
-		else{
-
+		else{ // O(1)
+			temp = new Node(head, nullptr, v);
+			head = temp;
 		}
 	}
 
 	void addEnd(int v) {
+		Node* temp;
 		//Case if list is empty
-		if(head == nullptr){
-			temp = new Node(nullptr, nullptr, v)
+		if(head == nullptr){ // O(1)
+			temp = new Node(nullptr, nullptr, v);
 			head = temp;
 			tail = temp;
-			delete temp;
 		}
-		else{
-
+		else{	// O(1)
+			Node* p = tail;
+			temp = new Node(nullptr, tail, v);
+			p->next = temp;
+			tail = temp;
 		}
 	}
 
-	void removeStart() {
+	void removeStart() { // O(1)
 		//Case if list is empty
-		if(head == nullptr){
+		if(head == nullptr){ // O(1)
 			return;
+		}
+		else{	//O(1)
+			Node* temp = head;
+			head = head->next;
+			//Checks to see if the element removed was the last element in the list
+			if (head == nullptr)
+				tail = nullptr;
+			delete temp;
 		}
 	}
 
 	void removeEnd() {
 		//Case if list is empty
-		if(head == nullptr){
+		if(head == nullptr){ // O(1)
 			return;
+		}
+		else{	// O(1)
+			Node* temp = tail;
+			if(tail->prev == nullptr){
+				tail = nullptr;
+				head = nullptr;
+				delete temp;
+			}
+			else{ // O(1)
+				tail = tail->prev;
+				tail->next = nullptr;
+				delete temp;
+			}
 		}
 	}
 
 	// insert v before pos. insert(0,v) would insert at the front.
 	void insert(int pos, int v) {
+		if(head == nullptr)
+			return;
+		else{
+			Node* temp;
+			Node* p = head;
+			for(int i = 0; i < pos - 1; i++){
+				if(p == nullptr)
+					return;
+				p = p->next;
+			}
+			temp = new Node(p->next, p, v);
+			p->next = temp;
+		}
 	}
 };
 
 int main() {
 	DoubleLinkedList a;
-	const uint32_t n = 1000000;
+	const uint32_t n = 100; //1000000
 	for (int i = 0; i < n; i++)
 		a.addStart(i);
+	//cout << a << '\n';
+
 	for (int i = 0; i < n; i++)
 		a.addEnd(i);
+	//cout << a << '\n';
+
 	for (int i = 0; i < 3*n/2; i++)
 		a.removeStart();
+	//cout << a << '\n';
 
 	for (int i = 0; i < n/2 - 5; i++)
 		a.removeEnd();
+	//cout << a << '\n';
 
-	cout << a << '\n';
 	for (int i = 0; i < 10; i++)
 		a.insert(1, i);
-	cout << a << '\n';
+	//cout << a << '\n';
+
 }
