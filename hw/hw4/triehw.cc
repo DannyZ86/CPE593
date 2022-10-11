2eb8655d9eab4085fd6283055ea5060cf46f64c3
Danny Desktop <dannyzatko@gmail.com>
2022-10-10 23:21:34 -0400

    Finished hw4 triedicetionary

--- hw/hw4/triehw.cc
+++ hw/hw4/triehw.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -9,6 +10,12 @@ using namespace std;
   Dictionary Link:
   https://githup.com/StevensDeptECE/CPE593/blob/master/data/dict.txt
   */
+
+/*
+  I used code from cplusplus.com as a reference when trying to walk through a file
+  https://cplusplus.com/doc/tutorial/files
+*/
+
 class TrieDictionary {
 private:
   class TrieNode {
@@ -16,17 +23,21 @@ private:
     // 0-25 represents each letter in the alphabet
     TrieNode* next[26];
     bool isWord;
+    //Initializeing all 26 index's as null because c++ does not do this
+    TrieNode(){
+      isWord = false;
+      for(int i = 0; i < 26; i++){
+        next[i] = nullptr;
+      }
+    }
   };
+
   TrieNode* root;
 
 public:
   //Constructor/Destructor
   TrieDictionary(){
-    //Initializeing all 26 index's as null because c++ does not do this
     root = new TrieNode();
-    for(int i = 0; i < 26; i++){
-      root[i] = nullptr;
-    }
   }
   ~TrieDictionary() {}
   //Copy Constructors
@@ -36,10 +47,12 @@ public:
   //Insert a word into the Dictionary
   void insert(string word){
     TrieNode* temp = root;
-    for(int i = 0; i < word.size(); i++){
-      char letter = word[i] - 'a';
-      if(temp->next[letter] == nullptr)
+    //Here the -1 is due to the fact that fstream adds a return value to the end of the string
+    for(int i = 0; i < word.size()-1; i++){
+      int letter = word[i] - 'a';
+      if(temp->next[letter] == nullptr){
         temp->next[letter] = new TrieNode();
+      }
       temp = temp->next[letter];
     }
     temp->isWord = true;
@@ -47,21 +60,31 @@ public:
 
   bool find(string word){
     TrieNode* temp = root;
-    for(int i = 0; i < word.size(); i++){
-      char letter = word[i] - 'a';
-      temp = temp->next[letter];
-      if(temp[letter] == nullptr)
+    //Here the -1 is due to the fact that fstream adds a return value to the end of the string
+    for(int i = 0; i < word.size()-1; i++){
+      int letter = word[i] - 'a';
+      //Conditional case incase the word is not a letter a - z
+      if(letter < 0 || letter >= 26){
         return false;
+      }
+      if(temp->next[letter] == nullptr){
+        return false;
+      }
+      temp = temp->next[letter];
     }
     return temp->isWord;
   }
 
   bool containsPrefix(string prefix){
     TrieNode* temp = root;
-    for(int i = 0; i < word.size(); i++){
-      char letter = word[i];
+    for(int i = 0; i < prefix.size(); i++){
+      int letter = prefix[i];
+      //Conditional case incase the word is not a letter a - z
+      if(letter < 0 || letter >= 26){
+        return false;
+      }
       temp = temp->next[letter];
-      if(temp[letter] == nullptr)
+      if(temp->next[letter] == nullptr)
         return false;
     }
     return true;
@@ -70,4 +93,28 @@ public:
 
 int main(){
   TrieDictionary treeDict;
+  string line;
+
+  //This will only work if the files are in your folder
+  //Storing the words into the Dictionary
+  ifstream dictionary ("dict.txt"); //Open file
+  if(dictionary.is_open()){
+    while( getline(dictionary, line) ){
+      treeDict.insert(line);
+    }
+    dictionary.close();
+  }
+
+  //Searching for words in dictionary
+  ifstream  spellcheck ("triespellcheck.txt"); //Open file
+  if(spellcheck.is_open()){
+    while( getline(spellcheck, line) ){
+      if(treeDict.find(line) == false)
+        cout << line << '\n';
+    }
+    spellcheck.close();
+
+  }
+
+
 }
