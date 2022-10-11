764fa09d6dd7260fa5de8a2059e271fe4544aecc
Daniel Zatko <dannyzatko@gmail.com>
2022-10-10 20:38:10 -0400

    added HW 4

---
+++ hw/hw4/triehw.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  Daniel Zatko
+  Homework 4
+  Trie Dictionary
+  Dictionary Link:
+  https://githup.com/StevensDeptECE/CPE593/blob/master/data/dict.txt
+  */
+class TrieDictionary {
+private:
+  class TrieNode {
+  public:
+    // 0-25 represents each letter in the alphabet
+    TrieNode* next[26];
+    bool isWord;
+  };
+  TrieNode* root;
+
+public:
+  //Constructor/Destructor
+  TrieDictionary(){
+    //Initializeing all 26 index's as null because c++ does not do this
+    for(int i = 0; i < 26; i++){
+      TrieDictionary.next[i] = nullptr;
+    }
+  }
+  ~TrieDictionary() {}
+  //Copy Constructors
+  TrieDictionary(const TrieDictionary& orig) = delete;
+  TrieDictionary& operator =(const TrieDictionary& orig) = delete;
+
+  //Insert a word into the Dictionary
+  void insert(string word){
+    TrieNode* temp = root;
+    for(int i = 0; i < word.size(); i++){
+      char letter = word[i] - 'a';
+      if(temp->next[letter] == nullptr)
+        temp->next[letter] = new TrieNode();
+      temp = temp->next[letter];
+    }
+    temp->isWord = true;
+  }
+
+  bool find(string word){
+    TrieNode* temp = root;
+    for(int i = 0; i < word.size(); i++){
+      char letter = word[i] - 'a';
+      temp = temp->next[letter];
+      if(temp[letter] == nullptr)
+        return false;
+    }
+    return temp->isWord;
+  }
+
+  bool containsPrefix(string prefix){
+    TrieNode* temp = root;
+    for(int i = 0; i < word.size(); i++){
+      char letter = word[i];
+      temp = temp->next[letter];
+      if(temp[letter] == nullptr)
+        return false;
+    }
+    return true;
+  }
+};
+
+int main(){
+  TrieDictionary treeDict = new TrieDictionary();
+}
