8463a35241e0b646f07249de41f5424aefbcbc87
Danny Desktop <dannyzatko@gmail.com>
2022-12-06 15:27:27 -0500

    hw6

---
+++ hw/hw6/gram.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+  Daniel Zatko
+  Homework 6
+  Gram Shmidt
+*/
+
+using namespace std;
+
+int generateMatrix(int n){
+}
+
+
+int main(){
+  int n;
+  cout << "what size n x n matrix would you like to create?" << endl;
+  cin >> n;
+  int[] matrix = generateMatrix(n);
+}
